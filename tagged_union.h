e11a1196629e9814d6e6dccf09fcf39d8a9071e5
wendyn-projects <wendyncoding@gmail.com>
2024-07-11 16:00:05 +0200

    I added macros for checking the tag and casting. I tried to make difference between tag-value & tag-label more obvious.

--- tagged_union.h
+++ tagged_union.h
@@ -11,10 +11,10 @@
 #define TAGGED_UNION_H_
 /*
  * Constructs the tagged-union instance.
- * @param tag enum label tag of the union member
+ * @param tag_label enum tag label of the union member
  * @param ... initialization of the union member
  */
-#define tu_new(tag, ...) { .tag_value = tag, .untagged_union.tag = __VA_ARGS__ }
+#define tu_new(tag_label, ...) { .tag_value = tag_label, .untagged_union.tag_label = __VA_ARGS__ }
 
 /**
  * `switch` wrapper that stores the pointer to the union for later use, which compiler should be able to optimize it out.
@@ -30,15 +30,15 @@
 
 /**
  * `case` wrapper that safely casts the right union member (from previously stored pointer) based on the tag.
- * @param tag tag enum **label**
+ * @param tag_label enum tag **label**
  * @param member_ptr_name pointer that will be created for the correct union member, compiler should be able to optimize it out.
  */
-#define tu_matches(tag, member_ptr_name) \
+#define tu_matches(tag_label, member_ptr_name) \
     break; \
 } \
-case tag: \
+case tag_label: \
 { \
-    tag##_t* const member_ptr_name = &__tu->untagged_union.tag;
+    tag_label##_t* const member_ptr_name = &__tu->untagged_union.tag_label;
 
 /**
  * `default` wrapper
@@ -54,9 +54,15 @@ default: \
  */
 #define tu_resolved } } }
 
-#define __tu_create_typedefs(tag, ...) typedef __VA_ARGS__ tag##_t;
-#define __tu_create_tags(tag, ...) tag,
-#define __tu_create_union_members(tag, ...) tag##_t tag;
+#define tu_check(tagged_union, tag) ((tagged_union).tag_value == (tag))
+#define tu_check_ptr(tagged_union_ptr, tag) ((tagged_union_ptr)->tag_value == (tag))
+
+#define tu_cast(tag_label, tagged_union) (assert(tu_check((tagged_union), tag_label)), (tagged_union).untagged_union.tag_label)
+#define tu_cast_ptr(tag_label, tagged_union_ptr) (tu_check_ptr(tagged_union_ptr, tag_label)? &(tagged_union_ptr)->untagged_union.tag_label : NULL)
+
+#define __tu_create_typedefs(tag_label, ...) typedef __VA_ARGS__ tag_label##_t;
+#define __tu_create_tags(tag_label, ...) tag_label,
+#define __tu_create_union_members(tag_label, ...) tag_label##_t tag_label;
 
 #define tagged_union(entries) \
 entries(__tu_create_typedefs,) \
